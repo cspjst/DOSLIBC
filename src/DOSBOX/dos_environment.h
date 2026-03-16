5ddac0a6409079c6546b6d16776c8244f6875e99
Sauce Code... <jeremy.thornton@nhs.net>
2026-03-16 15:05:10 +0000

    Rename header guard in dos_environment.h

---
+++ src/DOSBOX/dos_environment.h
@@ -0,0 +1,6 @@
+#ifndef DOS_ENVIRONMENT_H
+#define DOS_ENVIRONMENT_H
+
+bool dos_environment_is_dosbox();  
+
+#endif
--- src/DOSBOX/dos_environment_is_dosbox.h
+++
@@ -1,6 +0,0 @@
-#ifndef DOS_ENVIRONMENT_IS_DOSBOX_H
-#define DOS_ENVIRONMENT_IS_DOSBOX_H
-
-bool dos_environment_is_dosbox();  
-
-#endif
