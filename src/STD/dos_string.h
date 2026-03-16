78ef585b913389ba151e6e0d2c1fdebc6308f641
Sauce Code... <jeremy.thornton@nhs.net>
2026-03-16 13:59:05 +0000

    Refactor includes and update function signatures

--- src/STD/dos_string.h
+++ src/STD/dos_string.h
@@ -1,7 +1,11 @@
 #ifndef STRING_H
 #define STRING_H
 
-#include "dos_stddef.h"
+#ifdef POLICY_USE_DOS_STD
+    #include "../STD/dos_stddef.h"
+#else
+    #include <stddef.h>
+#endif
 
 // String manipulation
 // strcpy // copies one string to another
@@ -11,9 +15,9 @@
 // String examination
 size_t strlen(const char* str);                 // returns the length of a given string
 int strcmp(const char* s1, const char* s2);     // compares two strings
-int strncmp( const char* lhs, const char* rhs, size_t count); // compares a certain amount of characters of two strings
-char* strchr(const char* str, int ch);          // finds the first occurrence of a character
-char* strrchr(const char* str, int ch);         // finds the last occurrence of a character
+int strncmp(const char *s1, const char *s2, size_t n); // compares a certain amount of characters of two strings
+char* strchr(const char *s, int c);          // finds the first occurrence of a character
+char* strrchr(const char *s, int c);         // finds the last occurrence of a character
 // strspn // returns the length of the maximum initial segment that consists of only the characters found in another byte string
 // strcspn returns the length of the maximum initial segment that consists of only the characters not found in another byte string
 // strpbrk // finds the first location of any character in one string, in another string
@@ -22,11 +26,12 @@ char* strrchr(const char* str, int ch);         // finds the last occurrence of 
 
 // Character array manipulation
 // memchr   // searches an array for the first occurrence of a character
-int memcmp(const void* lhs, const void* rhs, size_t count);   // compares two buffers
-void* memset(void* dest, int ch, size_t count);   // fills a buffer with a character
-void* memcpy(void* dest, const void* src, size_t count);   // copies one buffer to another
+int memcmp(const void *s1, const void *s2, size_t n);   // compares two buffers
+void* memset(void *s, int c, size_t n);   // fills a buffer with a character
+void* memcpy(void *dest, const void *src, size_t n);   // copies one buffer to another
 // memmove  // moves one buffer to another
 // memccpy  // copies one buffer to another, stopping after the specified delimiter
+void* memmem(const void* haystack, size_t hsize, const void* needle, size_t nsize); // finds the start of the first occurrence of the substring needle of size nsize in the memory area haystack of size hsize
 
 // Miscellaneous
 const char* strerror(int errnum);
